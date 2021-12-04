292e7976e0994fd940d070d4e824d550e463553b
nashalex <46897024+nashalex@users.noreply.github.com>
2021-12-03 19:22:34 -0500

    now i enabled auto file generation

--- Course.py
+++ Course.py
@@ -44,12 +44,15 @@ class Course(object):
         # return max(filtered, key=lambda f: f.number).number
 
     def new_tex_file(self, file_type: File.FileType):
-        f = File.TexFile(course_identifier=self.course_info.identifier,
-                         file_type=file_type, number=1 + self.largest_file_number())
+        File.TexFile(course_identifier=self.course_info.identifier,
+                     file_type=file_type, number=1 + self.largest_file_number(file_type))
 
 
 if __name__ == "__main__":
     c = Course('COMP332D')
     print(c)
     print(c.files)
+    print('\n'.join(str(f) for f in c.files))
     print(c.largest_file_number(File.FileType.homework))
+    c.new_tex_file(File.FileType.homework)
+    print('\n'.join(str(f) for f in c.files))
